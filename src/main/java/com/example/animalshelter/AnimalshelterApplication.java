9bd5689685cf59ea7206b0741129956c621d8a0d
patka <patrycja.hyjek@gmail.com>
2021-08-19 11:38:31 +0200

    Initialized project.

---
+++ src/main/java/com/example/animalshelter/AnimalshelterApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalshelterApplication.class, args);
+	}
+
+}
