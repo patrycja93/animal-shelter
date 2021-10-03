beb2150bc7bd89afedbeab3e66b1b2fa8ba917b4
Andrzej Fornalik <afornalik2@gmail.com>
2021-10-03 10:25:23 +0200

    Added mocked values to H2 database

--- src/main/java/com/example/animalshelter/model/Animal.java
+++ src/main/java/com/example/animalshelter/model/Animal.java
@@ -1,17 +1,37 @@
 package com.example.animalshelter.model;
 
+import com.example.animalshelter.service.AnimalService;
 import lombok.Data;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
 
 /**
  * Keeps basic information about an animal
  */
 @Data
+@Entity
 public class Animal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column
     private int age;
+
+    @Column
+    @Enumerated(EnumType.STRING)
     private AnimalType type;
+
+    @Column
+    @Enumerated(EnumType.STRING)
     private AnimalGender gender;
+
+    @Column
+    @Enumerated(EnumType.STRING)
     private AnimalHealthStatus healthStatus;
 }
